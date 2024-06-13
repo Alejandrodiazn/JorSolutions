06a76b1c6f445b353495dcfa0a5e5a8de91c7d9d
alejandrodiazn <idiaz16@alumnos.uaq.mx>
2024-06-13 02:05:52 -0600

    page tsh fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Exo_2 } from "next/font/google";
 import "./globals.css";
 
-const exo2 = Exo_2({ subsets: ["latin"], weights: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], styles: ["normal", "italic"] });
+const exo2 = Exo_2({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], style: ["normal", "italic"] });
 
 export const metadata: Metadata = {
   title: "JorSolutions",
